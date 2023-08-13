ed4f35a25e29acb7de9b2f7bc99e9b6b0e34a95a
Strider <mezentsev.k@gmail.com>
2023-08-13 12:04:10 +0800

    Условные переменные. Урок2

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Условные переменные. Урок2");
+        System.out.println("--------------------------");
+        System.out.println("Задача №1");
+    }
+
+
+}
