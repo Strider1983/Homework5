a10fa92c4d2ec1bf5c8772283e2d6e7910a834ed
Strider <mezentsev.k@gmail.com>
2023-08-13 13:42:54 +0800

    Задача №5 выполнена

--- src/Main.java
+++ src/Main.java
@@ -53,5 +53,29 @@ public class Main {
         } else {
             System.out.println("Доставки нет.");
         }
+        System.out.println("--------------------------");
+        System.out.println("Задача №5");
+        int monthNumber = 11;
+        switch (monthNumber) {
+            case 1:
+            case 2:
+            case 12:
+                System.out.println(monthNumber + "-й месяц принадлежит к сезону зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println(monthNumber + "-й месяц принадлежит к сезону весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println(monthNumber + "-й месяц принадлежит к сезону лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println((monthNumber + "-й месяц принадлежит к сезону осень"));
+        }
     }
 }
